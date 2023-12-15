30577ed4561a3a8ed4eb68fc1af512f7aa951eca
liyang <2234333815@qq.com>
2023-12-15 13:38:26 +0800

    修改界面样式

--- Stack-Calculator/GUI.cpp
+++ Stack-Calculator/GUI.cpp
@@ -1,14 +1,15 @@
 #include "head.h"
+#include "resource.h"
 //界面按钮
-void button(int x, int y, int w, int h, int textSize, TCHAR* text){
+void button(int x, int y, int w, int h, int textSize, TCHAR* text, int roundRect) {
 	setbkmode(TRANSPARENT);//设置文字的背景透明
-	setfillcolor(0x878A80);//冷灰色
-	fillroundrect(x, y, x + w, y + h, 23, 23);//画出圆角矩形
-	settextstyle(textSize, 0, "幼圆");//设置字体格式
+	setfillcolor(0xAAAAAA);
+	fillroundrect(x, y, x + w, y + h, roundRect, roundRect);//画出圆角矩形
+	settextstyle(textSize, 0, "黑体");//设置字体格式
 	//使文本居中对齐
 	int tx = x + (w - textwidth(text)) / 2;
 	int ty = y + (h - textheight(text)) / 2;
-	settextcolor(0x363636);
+	settextcolor(0x333333);
 	outtextxy(tx, ty, text);//输出字符串
 }
 //初始化界面
@@ -18,19 +19,19 @@ void menu_init() {
 	SetWindowText(hwnd, "数学表达式求值");//修改窗口名字函数
 	/* 加载背景图片 */
 	IMAGE background;
-	loadimage(&background, ".\\image\\background.jpg", 1000, 563);
+	loadimage(&background, _T("IMAGE"), _T("background"), 1000, 563);
 	putimage(0, 0, &background);
-	TCHAR s1[50] = "从键盘输入";
-	TCHAR s2[50] = "从文件输入";
-	TCHAR s3[100] = "注：从键盘输入采用中缀表达式直接求值,从文件输入采用中缀转后缀表达式求值";
+	TCHAR s1[20] = "从键盘输入";
+	TCHAR s2[20] = "从文件输入";
+	TCHAR s3[110] = "注：从键盘输入采用中缀表达式直接求值,从文件输入采用中缀转后缀表达式求值";
 	//画包含文本的按钮
-	button(250, 250, 200, 100, 30, s1);
-	button(530, 250, 200, 100, 30, s2);
-	button(150, 0, 700, 25, 17, s3);
+	button(250, 250, 200, 100, 30, s1, 24);
+	button(530, 250, 200, 100, 30, s2, 24);
+	button(150, 0, 700, 25, 17, s3, 6);
 }
 //计算器界面
 void calcul_memu() {
-	setfillcolor(0xFFFFBB);
+	setfillcolor(0xFFFFFF);
 	solidrectangle(0, 100, 400, 600);
 	setlinestyle(PS_ENDCAP_ROUND | PS_JOIN_ROUND, 1);
 	setlinecolor(BLACK);
@@ -44,8 +45,8 @@ void calcul_memu() {
 	line(200, 100, 200, 600);
 	line(300, 100, 300, 600);
 	setbkmode(TRANSPARENT);//设置文字的背景透明
-	settextstyle(60, 35, "幼圆", 0, 0, 750, false, false, false);
-	settextcolor(0x46170B);
+	settextstyle(50, 30, "幼圆", 0, 0, 750, false, false, false);
+	settextcolor(0x333333);
 	outtextxy(35, 120, "C");
 	outtextxy(125, 120, "(");
 	outtextxy(240, 120, ")");
@@ -66,7 +67,7 @@ void calcul_memu() {
 	outtextxy(135, 520, "0");
 	outtextxy(235, 520, "=");
 	outtextxy(335, 520, "/");
-	setfillcolor(0xEEE58E);
+	setfillcolor(0xBBBBBB);
 	solidrectangle(0, 600, 400, 650);
 	settextstyle(0, 0, "幼圆", 0, 0, 1000, false, false, false);
 	outtextxy(140, 610, "返回主界面");
@@ -82,214 +83,215 @@ void mouse_info() {
 	ExMessage m;
 	while (true) {
 		setbkmode(OPAQUE);//用背景色充当文字填充背景
-		settextstyle(60, 20, "黑体", 0, 0, 0, false, false, false);
-		settextcolor(0x46170B);
+		settextstyle(60, 20, "幼圆", 0, 0, 0, false, false, false);
+		settextcolor(0x333333);
 		m = getmessage(EX_MOUSE | EX_KEY);
 		switch (m.message) {
 			//鼠标左键按下
-			case WM_LBUTTONDOWN:
-				if (str[0] == '\0') {
-					cleardevice();
-					calcul_memu();
-				}
-				if (m.x > 100 && m.x < 200 && m.y > 100 && m.y < 200) {
-					str[i] = '(';
-					++i; count++;
-				}
-				if (m.x > 200 && m.x < 300 && m.y > 100 && m.y < 200) {
-					str[i] = ')';
-					++i; count++;
-				}
-				if (m.x > 300 && m.x < 400 && m.y > 200 && m.y < 300) {
+		case WM_LBUTTONDOWN:
+			if (str[0] == '\0') {
+				cleardevice();
+				calcul_memu();
+			}
+			if (m.x > 100 && m.x < 200 && m.y > 100 && m.y < 200) {
+				str[i] = '(';
+				++i; count++;
+			}
+			if (m.x > 200 && m.x < 300 && m.y > 100 && m.y < 200) {
+				str[i] = ')';
+				++i; count++;
+			}
+			if (m.x > 300 && m.x < 400 && m.y > 200 && m.y < 300) {
 
-					str[i] = '+';
-					++i; count++;
-				}
-				if (m.x > 0 && m.x < 100 && m.y > 200 && m.y < 300) {
+				str[i] = '+';
+				++i; count++;
+			}
+			if (m.x > 0 && m.x < 100 && m.y > 200 && m.y < 300) {
 
-					str[i] = '7';
-					++i; count++;
+				str[i] = '7';
+				++i; count++;
+			}
+			if (m.x > 100 && m.x < 200 && m.y > 200 && m.y < 300) {
+				str[i] = '8';
+				++i; count++;
+			}
+			if (m.x > 200 && m.x < 300 && m.y > 200 && m.y < 300) {
+				str[i] = '9';
+				++i; count++;
+			}
+			if (m.x > 300 && m.x < 400 && m.y > 300 && m.y < 400) {
+				str[i] = '-';
+				++i; count++;
+			}
+			if (m.x > 0 && m.x < 100 && m.y > 300 && m.y < 400) {
+				str[i] = '4';
+				++i; count++;
+			}
+			if (m.x > 100 && m.x < 200 && m.y > 300 && m.y < 400) {
+				str[i] = '5';
+				++i; count++;
+			}
+			if (m.x > 200 && m.x < 300 && m.y > 300 && m.y < 400) {
+				str[i] = '6';
+				++i; count++;
+			}
+			if (m.x > 300 && m.x < 400 && m.y > 400 && m.y < 500) {
+				str[i] = '*';
+				++i; count++;
+			}
+			if (m.x > 0 && m.x < 100 && m.y > 400 && m.y < 500) {
+				str[i] = '1';
+				++i; count++;
+			}
+			if (m.x > 100 && m.x < 200 && m.y > 400 && m.y < 500) {
+				str[i] = '2';
+				++i; count++;
+			}
+			if (m.x > 200 && m.x < 300 && m.y > 400 && m.y < 500) {
+				str[i] = '3';
+				++i; count++;
+			}
+			if (m.x > 300 && m.x < 400 && m.y > 500 && m.y < 600) {
+				str[i] = '/';
+				++i; count++;
+			}
+			if (m.x > 0 && m.x < 120 && m.y>500 && m.y < 600) {
+				str[i] = '.';
+				++i; count++;
+			}
+			if (m.x > 100 && m.x < 200 && m.y > 500 && m.y < 600) {
+				str[i] = '0';
+				++i; count++;
+			}
+			//按'<-'键清除后一个，往后退
+			if (m.x > 300 && m.x < 400 && m.y > 100 && m.y < 200) {
+				//前面还有字符
+				if (i != 0) {
+					i--;
+					str[i] = '\0';
+					count--;
+					outtextxy(400 - count * 20 - 20, 20, " ");//把前一位打印的字符清空
 				}
-				if (m.x > 100 && m.x < 200 && m.y > 200 && m.y < 300) {
-					str[i] = '8';
-					++i; count++;
-				}
-				if (m.x > 200 && m.x < 300 && m.y > 200 && m.y < 300) {
-					str[i] = '9';
-					++i; count++;
-				}
-				if (m.x > 300 && m.x < 400 && m.y > 300 && m.y < 400) {
-					str[i] = '-';
-					++i; count++;
-				}
-				if (m.x > 0 && m.x < 100 && m.y > 300 && m.y < 400) {
-					str[i] = '4';
-					++i; count++;
-				}
-				if (m.x > 100 && m.x < 200 && m.y > 300 && m.y < 400) {
-					str[i] = '5';
-					++i; count++;
-				}
-				if (m.x > 200 && m.x < 300 && m.y > 300 && m.y < 400) {
-					str[i] = '6';
-					++i; count++;
-				}
-				if (m.x > 300 && m.x < 400 && m.y > 400 && m.y < 500) {
-					str[i] = '*';
-					++i; count++;
-				}
-				if (m.x > 0 && m.x < 100 && m.y > 400 && m.y < 500) {
-					str[i] = '1';
-					++i; count++;
-				}
-				if (m.x > 100 && m.x < 200 && m.y > 400 && m.y < 500) {
-					str[i] = '2';
-					++i; count++;
-				}
-				if (m.x > 200 && m.x < 300 && m.y > 400 && m.y < 500) {
-					str[i] = '3';
-					++i; count++;
-				}
-				if (m.x > 300 && m.x < 400 && m.y > 500 && m.y < 600) {
-					str[i] = '/';
-					++i; count++;
-				}
-				if (m.x > 0 && m.x < 120 && m.y>500 && m.y < 600) {
-					str[i] = '.';
-					++i; count++;
-				}
-				if (m.x > 100 && m.x < 200 && m.y > 500 && m.y < 600) {
-					str[i] = '0';
-					++i; count++;
-				}
-				//按'<-'键清除后一个，往后退
-				if (m.x > 300 && m.x < 400 && m.y > 100 && m.y < 200) {
-					//前面还有字符
-					if (i != 0) {
-						i--;
-						str[i] = '\0';
-						count--;
-						outtextxy(400 - count * 20 - 20, 20, " ");//把前一位打印的字符清空
-					}
-				}
-				//按C键清除所有字符
-				if (m.x > 0 && m.x < 100 && m.y > 100 && m.y < 200) {
-					i = 0;
-					count = 0;
+			}
+			//按C键清除所有字符
+			if (m.x > 0 && m.x < 100 && m.y > 100 && m.y < 200) {
+				i = 0;
+				count = 0;
+				memset(str, '\0', sizeof(str));//字符串清空
+				cleardevice();
+				calcul_memu();
+			}
+			//按“=”结束输入
+			if (m.x > 200 && m.x < 300 && m.y > 500 && m.y < 600) {
+				str[i] = '=';
+				str[i + 1] = '\0';
+				cleardevice();//界面清空
+				calcul_memu();
+				//判断表达式是否为空
+				if (strcmp(str, "=") == 0) {
+					settextstyle(60, 20, "幼圆", 0, 0, 0, false, false, false);
+					outtextxy(180, 20, "表达式为空！");
+					es.clear();//表达式栈清空
 					memset(str, '\0', sizeof(str));//字符串清空
-					cleardevice();
-					calcul_memu();
-				}
-				//按“=”结束输入
-				if (m.x > 200 && m.x < 300 && m.y > 500 && m.y < 600) {
-					str[i] = '=';
-					str[i + 1] = '\0';
-					cleardevice();//界面清空
-					calcul_memu();
-					//判断表达式是否为空
-					if (strcmp(str, "=") == 0) {
-						settextstyle(60, 20, "黑体", 0, 0, 0, false, false, false);
-						outtextxy(180, 20, "表达式为空！");
-						es.clear();//表达式栈清空
-						memset(str, '\0', sizeof(str));//字符串清空
-						i = 0;
-						count = 0;
-						output = true;
-						break;
-					}
-					//如果表达式非法
-					if (judge_infix(str) == 0) {
-						settextstyle(60, 20, "黑体", 0, 0, 0, false, false, false);
-						outtextxy(180, 20, "表达式非法！");
-						es.clear();//表达式栈清空
-						memset(str, '\0', sizeof(str));//字符串清空
-						i = 0;
-						count = 0;
-						output = true;
-						break;
-					}
-					strcpy_s(expression, str);//拷贝字符串到表达式
-					transfer();
-					//获得运算结果字符串
-					string s = to_string(in_getOutput());
-					strcpy_s(out, s.c_str());
-					settextstyle(60, 20, "黑体", 0, 0, 0, false, false, false);
-					outtextxy(400 - s.length() * 20, 20, out);
-					es1.clear();//表达式栈清空
-					memset(str, '\0', sizeof(str));//字符串清空
-					memset(expression, '\0', sizeof(expression));//表达式清空
-					memset(out, '\0', sizeof(out));//运算结果清空
-					memset(suf, '\0', sizeof(suf));//后缀表达式清空
 					i = 0;
 					count = 0;
 					output = true;
 					break;
 				}
-				if (m.x > 0 && m.x < 400 && m.y > 600 && m.y < 650) {
-					main();
+				//如果表达式非法
+				if (judge_infix(str) == 0) {
+					settextstyle(60, 20, "幼圆", 0, 0, 0, false, false, false);
+					outtextxy(180, 20, "表达式非法！");
+					es.clear();//表达式栈清空
+					memset(str, '\0', sizeof(str));//字符串清空
+					i = 0;
+					count = 0;
+					output = true;
+					break;
 				}
-				break;
-			//鼠标左键弹起
-			case WM_LBUTTONUP:
-				outtextxy(400 - count * 20, 20, str);
-				break;
-			//键盘按下
-			case WM_KEYDOWN:
-				if (m.vkcode == 0x30) {
-					str[i] = '0';
-					++i; count++;
-				}
-				if (m.vkcode == 0x31) {
-					str[i] = '1';
-					++i; count++;
-				}
-				if (m.vkcode == 0x32) {
-					str[i] = '2';
-					++i; count++;
-				}
-				if (m.vkcode == 0x33) {
-					str[i] = '3';
-					++i; count++;
-				}
-				if (m.vkcode == 0x34) {
-					str[i] = '4';
-					++i; count++;
-				}
-				if (m.vkcode == 0x35) {
-					str[i] = '5';
-					++i; count++;
-				}
-				if (m.vkcode == 0x36) {
-					str[i] = '6';
-					++i; count++;
-				}
-				if (m.vkcode == 0x37) {
-					str[i] = '7';
-					++i; count++;
-				}
-				if (m.vkcode == 0x38) {
-					str[i] = '8';
-					++i; count++;
-				}
-				if (m.vkcode == 0x39) {
-					str[i] = '9';
-					++i; count++;
-				}
-				//回退键
-				if (m.vkcode == VK_BACK) {
-					//前面还有字符
-					if (i != 0) {
-						i--;
-						str[i] = '\0';
-						count--;
-						outtextxy(400 - count * 20 - 20, 20, " ");//把前一位打印的字符清空
-					}
-				}
-				break;
-			//键盘弹起
-			case WM_KEYUP:
-				outtextxy(400 - count * 20, 20, str);
+				strcpy_s(expression, str);//拷贝字符串到表达式
+				transfer();
+				//获得运算结果字符串
+				string s = to_string(in_getOutput());
+				strcpy_s(out, s.c_str());
+				settextstyle(60, 20, "幼圆", 0, 0, 0, false, false, false);
+				int length = static_cast<int>(s.length());
+				outtextxy(400 -  length * 20, 20, out);
+				es1.clear();//表达式栈清空
+				memset(str, '\0', sizeof(str));//字符串清空
+				memset(expression, '\0', sizeof(expression));//表达式清空
+				memset(out, '\0', sizeof(out));//运算结果清空
+				memset(suf, '\0', sizeof(suf));//后缀表达式清空
+				i = 0;
+				count = 0;
+				output = true;
 				break;
 			}
+			if (m.x > 0 && m.x < 400 && m.y > 600 && m.y < 650) {
+				main();
+			}
+			break;
+			//鼠标左键弹起
+		case WM_LBUTTONUP:
+			outtextxy(400 - count * 20, 20, str);
+			break;
+			//键盘按下
+		case WM_KEYDOWN:
+			if (m.vkcode == 0x30) {
+				str[i] = '0';
+				++i; count++;
+			}
+			if (m.vkcode == 0x31) {
+				str[i] = '1';
+				++i; count++;
+			}
+			if (m.vkcode == 0x32) {
+				str[i] = '2';
+				++i; count++;
+			}
+			if (m.vkcode == 0x33) {
+				str[i] = '3';
+				++i; count++;
+			}
+			if (m.vkcode == 0x34) {
+				str[i] = '4';
+				++i; count++;
+			}
+			if (m.vkcode == 0x35) {
+				str[i] = '5';
+				++i; count++;
+			}
+			if (m.vkcode == 0x36) {
+				str[i] = '6';
+				++i; count++;
+			}
+			if (m.vkcode == 0x37) {
+				str[i] = '7';
+				++i; count++;
+			}
+			if (m.vkcode == 0x38) {
+				str[i] = '8';
+				++i; count++;
+			}
+			if (m.vkcode == 0x39) {
+				str[i] = '9';
+				++i; count++;
+			}
+			//回退键
+			if (m.vkcode == VK_BACK) {
+				//前面还有字符
+				if (i != 0) {
+					i--;
+					str[i] = '\0';
+					count--;
+					outtextxy(400 - count * 20 - 20, 20, " ");//把前一位打印的字符清空
+				}
+			}
+			break;
+			//键盘弹起
+		case WM_KEYUP:
+			outtextxy(400 - count * 20, 20, str);
+			break;
+		}
 	}
 }
--- Stack-Calculator/main.cpp
+++ Stack-Calculator/main.cpp
@@ -17,52 +17,52 @@ int main() {
 	while (true) {
 		msg = getmessage(EX_MOUSE | EX_KEY);//获取鼠标和键盘消息
 		switch (msg.message) {
-			case WM_LBUTTONDOWN://鼠标左键点击
-				if (msg.x >= 250 && msg.x <= 250 + 200 && msg.y >= 250 && msg.y <= 250 + 100) {
-					initgraph(400, 650);//初始化界面
-					HWND hwnd = GetHWnd();//获取窗口句柄函数；
-					SetWindowText(hwnd, "计算器");//修改窗口名字函数
-					setbkcolor(0xFAFFF0);//设置背景颜色
-					cleardevice();//用背景颜色刷新
-					calcul_memu();//计算器界面
-					mouse_info();//读取鼠标消息
-				}
-				if (msg.x >= 530 && msg.x <= 530 + 200 && msg.y >= 250 && msg.y <= 250 + 100) {
-					HWND hn = GetHWnd();//获取窗口句柄函数；
-					char* path = get_file_path();//获得文件路径
-					char suf[MaxSize] = "";//定义后缀表达式字符串
-					//如果文件路径存在
-					if (path) {
-						strcpy_s(expression, read(path).c_str());//将文件第一行的表达式字符串保存到数组中
-						//判断表达式是否为空
-						if (strcmp(expression, "") == 0) {
-							MessageBox(hn, "表达式为空！", NULL, MB_OK | MB_ICONERROR);
-							break;
+		case WM_LBUTTONDOWN://鼠标左键点击
+			if (msg.x >= 250 && msg.x <= 250 + 200 && msg.y >= 250 && msg.y <= 250 + 100) {
+				initgraph(400, 650);//初始化界面
+				HWND hwnd = GetHWnd();//获取窗口句柄函数；
+				SetWindowText(hwnd, "计算器");//修改窗口名字函数
+				setbkcolor(0xFFFFFF);//设置背景颜色
+				cleardevice();//用背景颜色刷新
+				calcul_memu();//计算器界面
+				mouse_info();//读取鼠标消息
+			}
+			if (msg.x >= 530 && msg.x <= 530 + 200 && msg.y >= 250 && msg.y <= 250 + 100) {
+				HWND hn = GetHWnd();//获取窗口句柄函数；
+				char* path = get_file_path();//获得文件路径
+				char suf[MaxSize] = "";//定义后缀表达式字符串
+				//如果文件路径存在
+				if (path) {
+					strcpy_s(expression, read(path).c_str());//将文件第一行的表达式字符串保存到数组中
+					//判断表达式是否为空
+					if (strcmp(expression, "") == 0) {
+						MessageBox(hn, "表达式为空！", NULL, MB_OK | MB_ICONERROR);
+						break;
+					}
+					//判断表达式是否非法
+					if (judge_infix(expression) == 0) {
+						MessageBox(hn, "表达式非法！", NULL, MB_OK | MB_ICONERROR);
+					}
+					else {
+						MessageBox(hn, expression, "表达式", MB_OK | MB_ICONASTERISK);//输出表达式到窗口
+						getInput();//将表达式字符串转化为后缀表达式栈
+						//将后缀表达式栈的内容拷贝到后缀表达式字符串
+						for (int i = 0; i <= es.top; i++) {
+							strcat_s(suf, es.stack[i]);
+							strcat_s(suf, " ");
 						}
-						//判断表达式是否非法
-						if (judge_infix(expression) == 0) {
-							MessageBox(hn, "表达式非法！", NULL, MB_OK | MB_ICONERROR);
-						}
-						else {
-							MessageBox(hn, expression, "表达式", MB_OK | MB_ICONASTERISK);//输出表达式到窗口
-							getInput();//将表达式字符串转化为后缀表达式栈
-							//将后缀表达式栈的内容拷贝到后缀表达式字符串
-							for (int i = 0; i <= es.top; i++) {
-								strcat_s(suf, es.stack[i]);
-								strcat_s(suf, " ");
-							}
-							MessageBoxA(hn, suf, "中缀表达式化为后缀表达式为：", MB_OK | MB_ICONASTERISK);//输出后缀表达式字符串到窗口
-							char out[MaxSize] = "";//定义运算结果字符串
-							string s = to_string(suf_getOutput());//获取运算结果字符串
-							strcpy_s(out, s.c_str());//拷贝字符串
-							MessageBox(hn, out, "运算结果为", MB_OK | MB_ICONASTERISK);//输出结果字符串到窗口
-							strcpy_s(expression, "");//表达式清空
-						}
+						MessageBoxA(hn, suf, "中缀表达式化为后缀表达式为：", MB_OK | MB_ICONASTERISK);//输出后缀表达式字符串到窗口
+						char out[MaxSize] = "";//定义运算结果字符串
+						string s = to_string(suf_getOutput());//获取运算结果字符串
+						strcpy_s(out, s.c_str());//拷贝字符串
+						MessageBox(hn, out, "运算结果为", MB_OK | MB_ICONASTERISK);//输出结果字符串到窗口
+						strcpy_s(expression, "");//表达式清空
 					}
 				}
-				break;
-			default:
-				break;
+			}
+			break;
+		default:
+			break;
 		}
 	}
 	return 0;
